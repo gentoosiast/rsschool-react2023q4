28b5428df7d9f89e4f467ddaa9af7e47fbcdf5a8
Sergey Koksharov <gentoosiastic@gmail.com>
2023-10-21 12:12:41 +0300

    fix(stylelint): configure Stylelint to require lowerCamelCase classnames in CSS modules

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -4,4 +4,17 @@
 export default {
   defaultSeverity: 'warning',
   extends: ['stylelint-config-standard', 'stylelint-config-clean-order'],
+  overrides: [
+    {
+      files: ['**/*.module.css'],
+      rules: {
+        'selector-class-pattern': [
+          '^[a-z]+([A-Z][a-zA-Z]+)*$',
+          {
+            message: (selector) => `Expected class selector "${selector}" to be lowerCamelCase`,
+          },
+        ],
+      },
+    },
+  ],
 };
