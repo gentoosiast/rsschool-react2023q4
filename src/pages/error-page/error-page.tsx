d7fc8c4c3b24f5ddffbcc310fc8b724d06ecff3b
Sergey Koksharov <gentoosiastic@gmail.com>
2023-10-25 17:09:05 +0300

    refactor: add error video to /public/

--- src/pages/error-page/error-page.tsx
+++ src/pages/error-page/error-page.tsx
@@ -14,7 +14,7 @@ export class ErrorPage extends Component {
           height="480"
           loop={true}
           muted
-          src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExNGc2cGN2MGkxOHNnZWkxaXM2MThkb25qdWx4Z202NmZibGk2bWFlaSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/YNzg7r0YCy31B6YANN/giphy.mp4"
+          src="/error-video.mp4"
           width="480"
         />
       </div>
