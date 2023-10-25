a207ebb1269bc894c8c14ebd3383fa516d6c3afc
Sergey Koksharov <gentoosiastic@gmail.com>
2023-10-25 17:06:31 +0300

    fix: move ErrorBoundary higher up the component tree

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,15 +1,9 @@
 import { Component, ReactNode } from 'react';
 
-import { ErrorBoundary } from '@/components/error-boundary';
-import { ErrorPage } from '@/pages/error-page';
 import { HomePage } from '@/pages/home-page';
 
 export class App extends Component {
   render(): ReactNode {
-    return (
-      <ErrorBoundary fallback={<ErrorPage />}>
-        <HomePage />
-      </ErrorBoundary>
-    );
+    return <HomePage />;
   }
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import { App } from './app';
+import { ErrorBoundary } from './components/error-boundary';
+import { ErrorPage } from './pages/error-page';
 
 import './global.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary fallback={<ErrorPage />}>
+      <App />
+    </ErrorBoundary>
   </StrictMode>,
 );
