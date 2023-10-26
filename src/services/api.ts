b3c0979070b12ec487f75f33e0fca747abc8cfb1
Sergey Koksharov <gentoosiastic@gmail.com>
2023-10-26 16:51:29 +0300

    refactor: apply DRY principle to code in api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -50,38 +50,34 @@ export type ApiResponse = Output<typeof ApiSchema>;
 
 export type Character = Output<typeof CharacterSchema>;
 
+const fetchData = async (url: string): Promise<ApiResponse | null> => {
+  try {
+    const response = await axios.get(url);
+
+    const data: unknown = response.data;
+
+    return parse(ApiSchema, data);
+  } catch (err) {
+    if (err instanceof ValiError) {
+      console.error(flatten(err));
+    } else {
+      console.error(err);
+    }
+    return null;
+  }
+};
+
 export const api = {
   async getAll(): Promise<ApiResponse | null> {
-    try {
-      const response = await axios.get(baseURL);
-
-      const data: unknown = response.data;
-
-      return parse(ApiSchema, data);
-    } catch (err) {
-      if (err instanceof ValiError) {
-        console.error(flatten(err));
-      } else {
-        console.error(err);
-      }
-      return null;
-    }
+    return fetchData(baseURL);
   },
 
   async getOne(query: string, page = 1): Promise<ApiResponse | null> {
-    try {
-      const response = await axios.get(`${baseURL}?name=${query}&page=${page}`);
+    const params = new URLSearchParams({
+      name: query,
+      page: page.toString(),
+    });
 
-      const data: unknown = response.data;
-
-      return parse(ApiSchema, data);
-    } catch (err) {
-      if (err instanceof ValiError) {
-        console.error(flatten(err));
-      } else {
-        console.error(err);
-      }
-      return null;
-    }
+    return fetchData(`${baseURL}?${params.toString()}`);
   },
 };
