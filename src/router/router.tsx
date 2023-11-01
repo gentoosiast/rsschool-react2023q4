e5acb7fe518fb10e27d93c4b5fed96ff2e71a177
Sergey Koksharov <gentoosiastic@gmail.com>
2023-11-01 20:21:00 +0300

    feat: implement 404 page

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import { HomePage } from '@/pages/home-page';
+import { NotFoundPage } from '@/pages/not-found-page';
 
 export const router = createBrowserRouter([
   {
     element: <HomePage />,
     path: '/',
+  },
+  {
+    element: <NotFoundPage />,
+    path: '*',
   },
 ]);
