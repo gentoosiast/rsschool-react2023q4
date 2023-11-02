99132af76e210b753b727354e1819d78d623b2a1
Sergey Koksharov <gentoosiastic@gmail.com>
2023-11-02 10:01:34 +0300

    feat: add 'details' to search parameters when the card is clicked

--- src/components/character-list-item/character-list-item.tsx
+++ src/components/character-list-item/character-list-item.tsx
@@ -1,6 +1,9 @@
 import type { JSX } from 'react';
+import { Link, useSearchParams } from 'react-router-dom';
 
 import type { Character } from '@/services/api';
+
+import { getLink } from '@/lib/search-params';
 
 import styles from './character-list-item.module.css';
 
@@ -9,29 +12,33 @@ type Props = {
 };
 
 export function CharacterListItem({
-  character: { gender, image, location, name, origin, species, status },
+  character: { gender, id, image, location, name, origin, species, status },
 }: Props): JSX.Element {
+  const [searchParams] = useSearchParams();
+
   return (
     <article className={styles.card}>
-      <img alt={name} height="300" src={image} width="300" />
-      <h2 className={styles.cardHeading}>{name}</h2>
-      <div className={styles.cardInfo}>
-        <p>
-          <span>Status:</span> {status}
-        </p>
-        <p>
-          <span>Species:</span> {species}
-        </p>
-        <p>
-          <span>Gender:</span> {gender}
-        </p>
-        <p>
-          <span>Origin:</span> {origin}
-        </p>
-        <p>
-          <span>Location:</span> {location}
-        </p>
-      </div>
+      <Link className={styles.cardLink} to={getLink(searchParams, { details: `${id}` })}>
+        <img alt={name} height="300" src={image} width="300" />
+        <h2 className={styles.cardHeading}>{name}</h2>
+        <div className={styles.cardInfo}>
+          <p>
+            <span>Status:</span> {status}
+          </p>
+          <p>
+            <span>Species:</span> {species}
+          </p>
+          <p>
+            <span>Gender:</span> {gender}
+          </p>
+          <p>
+            <span>Origin:</span> {origin}
+          </p>
+          <p>
+            <span>Location:</span> {location}
+          </p>
+        </div>
+      </Link>
     </article>
   );
 }
