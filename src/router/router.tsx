7b75f82860eb485dc863a8e4dd4ce010a406c958
Sergey Koksharov <gentoosiastic@gmail.com>
2023-11-02 23:35:39 +0300

    feat: wrap all routes inside ErrorBoundary

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import { CharacterDetails } from '@/components/character-details';
+import { ErrorBoundary } from '@/components/error-boundary';
+import { ErrorPage } from '@/pages/error-page';
 import { HomePage } from '@/pages/home-page';
 import { NotFoundPage } from '@/pages/not-found-page';
 
-export const router = createBrowserRouter([
+const routes = [
   {
     children: [
       {
@@ -19,4 +21,9 @@ export const router = createBrowserRouter([
     element: <NotFoundPage />,
     path: '*',
   },
-]);
+].map((route) => ({
+  ...route,
+  element: <ErrorBoundary fallback={<ErrorPage />}>{route.element}</ErrorBoundary>,
+}));
+
+export const router = createBrowserRouter(routes);
