0c0997e5428bea127dfd2292ce51458eef419199
Sergey Koksharov <gentoosiastic@gmail.com>
2023-11-08 16:18:13 +0300

    feat: install stylelint-config-css-modules

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -3,21 +3,15 @@
  */
 export default {
   defaultSeverity: 'warning',
-  extends: ['stylelint-config-standard', 'stylelint-config-clean-order'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-css-modules',
+    'stylelint-config-clean-order',
+  ],
   overrides: [
     {
       files: ['**/*.module.css'],
       rules: {
-        'property-no-unknown': [
-          true,
-          {
-            ignoreProperties: [
-              // CSS Modules composition
-              // https://github.com/css-modules/css-modules#composition
-              'composes',
-            ],
-          },
-        ],
         'selector-class-pattern': [
           '^[a-z]+([A-Z][a-zA-Z]+)*$',
           {
