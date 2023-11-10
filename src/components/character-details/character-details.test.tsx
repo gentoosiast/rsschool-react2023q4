081335789a2948819a6c78125a4ee678f86fc516
Sergey Koksharov <gentoosiastic@gmail.com>
2023-11-10 18:21:18 +0300

    feat: test that detailed card data is correctly displayed

--- src/components/character-details/character-details.test.tsx
+++ src/components/character-details/character-details.test.tsx
@@ -1,6 +1,6 @@
 import { MemoryRouter } from 'react-router-dom';
 
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import { setupServer } from 'msw/node';
 import { afterAll, afterEach, beforeAll, describe, expect, it } from 'vitest';
 
@@ -31,6 +31,23 @@ describe('CharacterDetails', () => {
     );
 
     const cardHeading = screen.getByRole('heading', { level: 2, name: /loading…/i });
+
+    expect(cardHeading).toBeInTheDocument();
+  });
+
+  it('should correctly display the detailed card data', async () => {
+    render(
+      <MemoryRouter initialEntries={['/?details=8']}>
+        <CharacterDetails />
+      </MemoryRouter>,
+    );
+
+    const card = screen.getByTestId('details-card');
+
+    const cardHeading = await within(card).findByRole('heading', {
+      level: 2,
+      name: /adjudicator rick/i,
+    });
 
     expect(cardHeading).toBeInTheDocument();
   });
