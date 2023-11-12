89031ffaad5951f5576104d7726c9969064681a3
Sergey Koksharov <gentoosiastic@gmail.com>
2023-11-12 09:49:08 +0300

    fix: return correct data & headers from msw hanlders

--- src/services/msw/handlers.ts
+++ src/services/msw/handlers.ts
@@ -4,10 +4,13 @@ import { apiResponseMock, characterMock } from './mocks';
 
 export const handlers = [
   http.get(/\/character$/, () => {
-    return HttpResponse.json(apiResponseMock, { headers: { 'x-total-count': '3' }, status: 200 });
+    return HttpResponse.json(apiResponseMock.characters, {
+      headers: { 'x-total-count': '3' },
+      status: 200,
+    });
   }),
   http.get(/\/character\/8/, () => {
-    return HttpResponse.json(characterMock, { status: 200 });
+    return HttpResponse.json(characterMock, { headers: {}, status: 200 });
   }),
   http.get(/rickandmortyapi\.com/, () => {
     console.error('IMAGE CDN');
