ff2b92761a95a40454b7ea5ed7c547b46996328c
Sergey Koksharov <gentoosiastic@gmail.com>
2023-11-18 19:33:47 +0300

    feat: improve MSW, now it returns number of cards based on provided limit

--- src/tests/mocks/mocks.ts
+++ src/tests/mocks/mocks.ts
@@ -1,4 +1,10 @@
+import { parse } from 'valibot';
+
 import type { ApiResponse, Character } from '@/store/api';
+
+import { ApiSchema } from '@/store/api';
+
+import characterData from './characters.json';
 
 export const characterMock: Character = {
   created: '2017-11-04T20:03:34.737Z',
@@ -12,44 +18,9 @@ export const characterMock: Character = {
   type: '',
 };
 
-export const charactersMock: Character[] = [
-  {
-    created: '2017-11-04T18:48:46.250Z',
-    gender: 'Male',
-    id: 1,
-    location: 'Citadel of Ricks',
-    name: 'Rick Sanchez',
-    origin: 'Earth (C-137)',
-    species: 'Human',
-    status: 'Alive',
-    type: '',
-  },
-
-  {
-    created: '2017-11-04T18:50:21.651Z',
-    gender: 'Male',
-    id: 2,
-    location: 'Citadel of Ricks',
-    name: 'Morty Smith',
-    origin: 'unknown',
-    species: 'Human',
-    status: 'Alive',
-    type: '',
-  },
-  {
-    created: '2017-11-04T20:03:34.737Z',
-    gender: 'Male',
-    id: 8,
-    location: 'Citadel of Ricks',
-    name: 'Adjudicator Rick',
-    origin: 'unknown',
-    species: 'Human',
-    status: 'Dead',
-    type: '',
-  },
-];
+export const charactersMock = parse(ApiSchema, characterData);
 
 export const apiResponseMock: ApiResponse = {
   characters: charactersMock,
-  total: 3,
+  total: charactersMock.length,
 };
--- src/tests/msw/handlers.ts
+++ src/tests/msw/handlers.ts
@@ -1,11 +1,20 @@
 import { HttpResponse, http } from 'msw';
 
-import { apiResponseMock, characterMock } from '@/tests/mocks';
+import { DEFAULT_ITEMS_PER_PAGE } from '@/store/api/constants';
+import { characterMock, charactersMock } from '@/tests/mocks';
 
 export const handlers = [
-  http.get(/\/character$/, () => {
-    return HttpResponse.json(apiResponseMock.characters, {
-      headers: { 'x-total-count': '3' },
+  http.get(/\/character$/, ({ request }) => {
+    const url = new URL(request.url);
+    const limitSearchParam = url.searchParams.get('_limit') ?? '0';
+
+    const limit = parseInt(limitSearchParam, 10) || DEFAULT_ITEMS_PER_PAGE;
+
+    const characters = charactersMock.slice(0, limit);
+    const total = characters.length;
+
+    return HttpResponse.json(characters, {
+      headers: { 'x-total-count': `${total}` },
       status: 200,
     });
   }),
