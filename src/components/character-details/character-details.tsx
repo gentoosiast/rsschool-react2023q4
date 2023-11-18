c3369c599a9675030e0e2a114b1db6fe8b626d57
Sergey Koksharov <gentoosiastic@gmail.com>
2023-11-18 21:53:49 +0300

    fix: fix details card to use loading flag Redux store

--- src/components/character-details/character-details.tsx
+++ src/components/character-details/character-details.tsx
@@ -5,10 +5,12 @@ import { skipToken } from '@reduxjs/toolkit/dist/query';
 import { CharacterDetailsCard } from '@/components/character-details-card';
 import { useAppSearchParams } from '@/hooks/use-app-search-params';
 import { useGetByIdQuery } from '@/store/api';
+import { useAppSelector } from '@/store/hooks';
 
 export function CharacterDetails(): ReactNode {
   const { deleteParam, details } = useAppSearchParams();
-  const { data, isError, isLoading } = useGetByIdQuery(details ?? skipToken);
+  const { data, isError } = useGetByIdQuery(details ?? skipToken);
+  const isLoading = useAppSelector((state) => state.settings.isDetailsLoading);
 
   function handleCloseDetails(): void {
     deleteParam('details');
