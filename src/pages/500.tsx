f540b65aeb45aada5b7f9c4e5f94529ada5ae788
Sergey Koksharov <gentoosiastic@gmail.com>
2023-11-26 15:31:22 +0300

    feat: add 500 (Internal Server Error) page

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -3,15 +3,15 @@ import Link from 'next/link';
 
 import styles from '@/styles/404-500.module.css';
 
-export default function NotFoundPage(): JSX.Element {
+export default function ServerErrorPage(): JSX.Element {
   return (
     <div className={styles.page}>
-      <h1 className={styles.heading}>Page not found</h1>
+      <h1 className={styles.heading}>Internal Server Error</h1>
       <Image
-        alt="Rick and Morty in unknown galaxy"
-        height={684}
-        src="/rick-morty-404.webp"
-        width={700}
+        alt="Rick and Morty in unpleasant situation"
+        height={438}
+        src="/rick-morty-500.webp"
+        width={780}
       />
       <Link className={styles.link} href="/">
         Return home
