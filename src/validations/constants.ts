374ba9086e18abfaf76a71f25759f4ea907f9c77
Sergey Koksharov <gentoosiastic@gmail.com>
2023-12-02 17:33:04 +0300

    feat: improve schema and require minimum password length of 8 chars

--- src/validations/constants.ts
+++ src/validations/constants.ts
@@ -1,2 +1,3 @@
 export const MIN_AGE = 1;
 export const MAX_AGE = 130;
+export const MIN_PASSWORD_LENGTH = 8;
--- src/validations/schema.ts
+++ src/validations/schema.ts
@@ -1,29 +1,35 @@
 import { getNames } from 'country-list';
 import { boolean, mixed, number, object, ref, string } from 'yup';
 
-import { MAX_AGE, MIN_AGE } from './constants';
+import { MAX_AGE, MIN_AGE, MIN_PASSWORD_LENGTH } from './constants';
 import { isFileSizeOK, isFiletypeAllowed } from './test-functions';
 
 export const formSchema = object().shape({
-  age: number().required().min(MIN_AGE).max(MAX_AGE).integer(),
-  country: string().required().oneOf(getNames(), 'should be valid country'),
+  age: number()
+    .required()
+    .min(MIN_AGE)
+    .max(MAX_AGE)
+    .integer()
+    .typeError('Age must be a positive number'),
+  country: string().required().oneOf(getNames(), 'Should be valid country'),
   email: string().required().email(),
   gender: string().oneOf(['male', 'female', 'nonbinary']).required(),
   name: string()
     .required()
-    .matches(/\p{Lu}/u, 'name must start with a capital letter'),
+    .matches(/\p{Lu}/u, 'Name must start with a capital letter'),
   password: string()
     .required()
-    .matches(/\d/, 'password must contain a number')
-    .matches(/\p{Lu}/u, 'password must contain a capital letter')
-    .matches(/\p{Ll}/u, 'password must contain a lowercase letter')
-    .matches(/[@$!%*#?&^\-+:|/\\]/, 'password must contain a special character'),
+    .min(MIN_PASSWORD_LENGTH)
+    .matches(/\d/, 'Password must contain a number')
+    .matches(/\p{Lu}/u, 'Password must contain a capital letter')
+    .matches(/\p{Ll}/u, 'Password must contain a lowercase letter')
+    .matches(/[@$!%*#?&^\-+:|/\\]/, 'Password must contain a special character'),
   password2: string()
     .required()
     .oneOf([ref('password')], 'Passwords do not match'),
-  picture: mixed()
+  picture: mixed((input): input is FileList => input instanceof FileList)
     .required()
     .test('filesize', 'Image filesize is too big', isFileSizeOK)
     .test('filetype', 'Only PNG and JPG images are allowed', isFiletypeAllowed),
-  tos: boolean().required().isTrue(),
+  tos: boolean().required('You must accept the Terms of Service').isTrue(),
 });
