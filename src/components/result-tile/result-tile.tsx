125116caf6b56b4a8149e7f301f4d0b138e46529
Sergey Koksharov <gentoosiastic@gmail.com>
2023-12-02 16:50:23 +0300

    feat: improve styles of ResultTile

--- src/components/result-tile/result-tile.tsx
+++ src/components/result-tile/result-tile.tsx
@@ -1,5 +1,7 @@
 import { JSX } from 'react';
 
+import { clsx } from 'clsx';
+
 import type { FormState } from '@/store';
 
 import styles from './result-tile.module.css';
@@ -12,38 +14,52 @@ type Props = {
 
 export const ResultTile = ({ className = '', formData, formName }: Props): JSX.Element => {
   if (!formData) {
-    return <h1>No data</h1>;
+    return (
+      <article className={clsx(styles.tile, className)}>
+        <h2 className={styles.heading}>{formName}</h2>
+        <p className={styles.tileField}>
+          <span className={styles.fieldHeading}>No Data Available</span>
+        </p>
+      </article>
+    );
   }
 
   const { age, country, email, gender, name, password, picture, tos } = formData;
 
   return (
-    <article className={className}>
-      <h2>{formName}</h2>
-      <p>
-        <span className={styles.fieldHeading}>Name:</span> {name}
+    <article className={clsx(styles.tile, className)}>
+      <h2 className={styles.heading}>{formName}</h2>
+
+      <p className={styles.tileField}>
+        <span className={styles.fieldHeading}>Name:</span>{' '}
+        <span className={styles.fieldValue}>{name}</span>
       </p>
-      <p>
-        <span className={styles.fieldHeading}>Age:</span> {age}
+      <p className={styles.tileField}>
+        <span className={styles.fieldHeading}>Age:</span>{' '}
+        <span className={styles.fieldValue}>{age}</span>
       </p>
-      <p>
-        <span className={styles.fieldHeading}>Gender:</span> {gender}
+      <p className={styles.tileField}>
+        <span className={styles.fieldHeading}>Gender:</span>{' '}
+        <span className={styles.fieldValue}>{gender}</span>
       </p>
-      <p>
-        <span className={styles.fieldHeading}>Email:</span> {email}
+      <p className={styles.tileField}>
+        <span className={styles.fieldHeading}>Email:</span>{' '}
+        <span className={styles.fieldValue}>{email}</span>
       </p>
-      <p>
-        <span className={styles.fieldHeading}>Password:</span> {password}
+      <p className={styles.tileField}>
+        <span className={styles.fieldHeading}>Password:</span>{' '}
+        <span className={styles.fieldValue}>{password}</span>
       </p>
-      <p>
-        <span className={styles.fieldHeading}>Country:</span> {country}
+      <p className={styles.tileField}>
+        <span className={styles.fieldHeading}>Country:</span>{' '}
+        <span className={styles.fieldValue}>{country}</span>
       </p>
-      <p>
-        <span className={styles.fieldHeading}>Picture:</span>
+      <p className={styles.tileField}>
+        <span className={styles.fieldHeading}>Accepted Terms of Service:</span>{' '}
+        <span className={styles.fieldValue}>{tos ? 'yes' : 'no'}</span>
+      </p>
+      <p className={styles.tileField}>
         <img alt={name} src={picture} />
-      </p>
-      <p>
-        <span className={styles.fieldHeading}>Accepted Terms of Service:</span> {tos}
       </p>
     </article>
   );
