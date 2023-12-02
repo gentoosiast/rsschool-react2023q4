193b76aaa3100878d07cacc11c74e84b79da6211
Sergey Koksharov <gentoosiastic@gmail.com>
2023-12-02 18:10:27 +0300

    fix: use constant with minimum password length in password-strength module

--- src/lib/password-strength.ts
+++ src/lib/password-strength.ts
@@ -1,3 +1,5 @@
+import { MIN_PASSWORD_LENGTH } from '@/validations/constants';
+
 const isSufficientLength = (minLength: number): ((password: string) => boolean) => {
   return (password: string) => password.length >= minLength;
 };
@@ -7,15 +9,13 @@ const checkRegex = (regex: RegExp): ((password: string) => boolean) => {
 };
 
 export const getPasswordStrength = (password: string): number => {
-  const RECOMMENDED_PASSWORD_LENGTH = 8;
-
   const hasNumberRegex = /\d/;
   const hasCapitalLetterRegex = /\p{Lu}/u;
   const hasLowerCaseLetterRegex = /\p{Ll}/u;
   const hasSpecialSymbolRegex = /[@$!%*#?&^\-+:|/\\]/;
 
   const score = [
-    isSufficientLength(RECOMMENDED_PASSWORD_LENGTH),
+    isSufficientLength(MIN_PASSWORD_LENGTH),
     checkRegex(hasNumberRegex),
     checkRegex(hasCapitalLetterRegex),
     checkRegex(hasLowerCaseLetterRegex),
