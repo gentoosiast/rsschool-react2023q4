7747b0fa2fd3001d694aae31c1b81285b1b4686f
Sergey Koksharov <gentoosiastic@gmail.com>
2023-12-03 10:41:32 +0300

    feat: create better styles for color indication of password strength

--- src/components/password-input/password-input.tsx
+++ src/components/password-input/password-input.tsx
@@ -1,13 +1,17 @@
 import type { ChangeEvent, InputHTMLAttributes } from 'react';
 import { forwardRef, useState } from 'react';
 
+import { clsx } from 'clsx';
+
 import { getPasswordStrength } from '@/lib/password-strength';
+
+import styles from './password-input.module.css';
 
 type Props = InputHTMLAttributes<HTMLInputElement>;
 
 export const PasswordInput = forwardRef<HTMLInputElement, Props>(({ onChange, ...props }, ref) => {
   const [passwordStrength, setPasswordStrength] = useState(0);
-  const strengthColors = ['transparent', '#fecaca', '#fed7aa', '#d9f99d', '#86efac', '#4ade80'];
+  const strengthColors = ['', '#ec4899', '#f59e0b', '#fde047', '#a3e635', '#4ade80'];
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     const passwordStrength = getPasswordStrength(event.target.value);
@@ -19,11 +23,12 @@ export const PasswordInput = forwardRef<HTMLInputElement, Props>(({ onChange, ..
 
   return (
     <input
+      {...props}
+      className={clsx(props.className, styles.passwordInput)}
       onChange={handleChange}
       ref={ref}
-      style={{ borderBlockEnd: `4px solid ${strengthColors[passwordStrength]}` }}
+      style={{ borderBlockEndColor: `${strengthColors[passwordStrength]}` }}
       type="password"
-      {...props}
     />
   );
 });
